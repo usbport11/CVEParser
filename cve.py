57d9c4b388248c1795fd82fef218fff9c8aae101
usbport11 <51235513+usbport11@users.noreply.github.com>
2023-02-20 19:51:41 +0300

    Update cve.py

--- cve.py
+++ cve.py
@@ -5,10 +5,11 @@ import re
 from bs4 import BeautifulSoup
 import psycopg2
 from psycopg2 import Error
+import time
 
 def getTopCVEList_cvetrends():
   link = 'https://cvetrends.com/api/cves/24hrs'
-  r = requests.get(link)
+  r = requests.get(link, proxies=proxies)
   soup = BeautifulSoup(r.text, "lxml")
   regex = re.findall(r'"cve": "CVE-\d{4}-\d{4,8}"', soup.get_text())
   topCVEList = []
@@ -21,7 +22,7 @@ def getTopCVEList_opencve():
   url2 = 'https://www.opencve.io/login'
   csrf_token = ''
   s = requests.Session()
-  response = s.get(url1)
+  response = s.get(url1, proxies=proxies)
   soup = BeautifulSoup(response.text, 'lxml')
 
   # Get CSRF
@@ -39,13 +40,14 @@ def getTopCVEList_opencve():
       'csrf_token': csrf_token,
     },
     headers={'referer': 'https://www.opencve.io/login'},
-    verify=False
+    verify=False,
+    proxies=proxies
   )
   # Get new CVE
   cve_line = []
   for page_num in range(1, 20):
     pagination = f'https://www.opencve.io/?page={page_num}'
-    resp = s.get(pagination)
+    resp = s.get(pagination, proxies=proxies)
     parse = BeautifulSoup(resp.text, 'lxml')
     for cve in parse.find_all('h3', class_='timeline-header'):
       index = cve.text.find('has changed')
@@ -78,7 +80,7 @@ def getCVEDataSelf(cve):
 
   url = "https://services.nvd.nist.gov/rest/json/cves/2.0?cveId=" + cve
   try:
-    res = requests.get(url).json()
+    res = requests.get(url, proxies=proxies).json()
   except:
     print("Error while request to cve base")
     return
@@ -96,7 +98,7 @@ def getCVEDataSelf(cve):
 
   metrics = cve['metrics']
   if len(metrics) >= 1:
-    if 'cvssMetricsV31' in metrics:
+    if 'cvssMetricV31' in metrics:
       metrics = metrics['cvssMetricV31'][0]['cvssData']
     elif 'cvssMetricV30' in metrics:
       metrics = metrics['cvssMetricV30'][0]['cvssData']
@@ -129,8 +131,17 @@ def getCVEDataSelf(cve):
   finally:
     cursor.close()
     print("%s add to database"%(name))
+    
+http_proxy  = "http://192.168.1.1:8080"
+
+proxies = {
+  "http"  : http_proxy,
+  "https" : http_proxy
+}
 
 CVEList = getTopCVEList_cvetrends()
 for cve in CVEList:
   print(cve + " processing ...")
   getCVEDataSelf(cve)
+  #if future need sleep only after get new CVE data
+  time.sleep(5)
