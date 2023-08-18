ae6c8100abc00ddfb2659caa9cc720ea5fcc4e72
usbport11 <51235513+usbport11@users.noreply.github.com>
2023-08-18 21:17:01 +0300

    Update cve.py

--- cve.py
+++ cve.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 import psycopg2
 from psycopg2 import Error
 import time
+import json
 
 def getTopCVEList_cvetrends():
   link = 'https://cvetrends.com/api/cves/24hrs'
@@ -35,17 +36,18 @@ def getTopCVEList_opencve():
   s.post(
     url2,
     data={
-      'username': 'someusername',
-      'password': 'somepassword',
+      'username': 'user',
+      'password': 'pass',
       'csrf_token': csrf_token,
     },
     headers={'referer': 'https://www.opencve.io/login'},
     verify=False,
     proxies=proxies
   )
+
   # Get new CVE
   cve_line = []
-  for page_num in range(1, 20):
+  for page_num in range(1, 10):
     pagination = f'https://www.opencve.io/?page={page_num}'
     resp = s.get(pagination, proxies=proxies)
     parse = BeautifulSoup(resp.text, 'lxml')
@@ -60,41 +62,84 @@ def getTopCVEList_opencve():
       cve_line_no_replic.append(item[:-1])
   return cve_line_no_replic
 
-def getCVEDataSelf(cve):
-  sql = "select count(*) from list where name = '" + cve + "'"
-  count = 0
-  try:
-    conn = psycopg2.connect(dbname='cve', user='cve', password='cve', host='localhost')
-    cursor = conn.cursor()
-    cursor.execute(sql, cve)
-    count = cursor.fetchone()[0]
-    conn.commit()
-  except (Exception, Error) as error:
-    print(error)
-  finally:
-    cursor.close()
+def getLastCVE_circl():
+  link = 'https://cve.circl.lu/api/last'
+  r = requests.get(link, proxies=proxies)
+  data = r.json()
+  cves = []
+  for item in data:
+    cves.append(item['id'])
+  return cves
 
-  if count >= 1:
-   print("%s alredy added to database"%(cve))
-   return
+def getCitrixCVEList():
+  list = []
+  link = "https://support.citrix.com/knowledge-center/search/#/All%20Products?ct=Security%20Bulletins&searchText=&sortBy=Created%20date&pageIndex=1"
+  r = requests.get(link, proxies=proxies)
+  soup = BeautifulSoup(r.text, "lxml")
+  ul = soup.find('ul')
+  lis = ul.find_all('li')
+  i = 0
+  limit = 3
+  for li in lis:
+    print(li.find('a', href=True)['href'])
+    i = i + 1
+    if i >= limit:
+      break
 
+def getCitrixCVE(link):
+  r = requests.get(link, proxies=proxies)
+  soup = BeautifulSoup(r.text, "lxml")
+  severity = soup.select_one('div.article-meta-info > span.category > span.severity').text
+  created = soup.find("meta", attrs={"name":"creationDate"})['content']
+  div = soup.find('div', {'data-swapid':'SecurityBulletinVulnerabilitiesList'})
+  table = div.find('table')
+  table_head = table.find('thead')
+  row = table_head.find('tr')
+  cols = row.find_all('th')
+  descrPos = 0
+  scorePos = 0
+  i = 0
+  for col in cols:
+    if col.text == 'Description':
+      descrPos = i
+    if col.text == 'CVSS':
+      scorePos = i
+    i = i + 1
+  table_body = table.find('tbody')
+  rows = table_body.find_all('tr')
+  for row in rows:
+    cols = row.find_all('td')
+    id = cols[0].text
+    descr = cols[descrPos].text
+    score = cols[scorePos].find('a').text
+    print(id)
+    print(descr)
+    print(score)
+    print(severity)
+    print(created)
+    print("=====================")
+
+def getCVEData(cve):
   url = "https://services.nvd.nist.gov/rest/json/cves/2.0?cveId=" + cve
   try:
     res = requests.get(url, proxies=proxies).json()
   except:
     print("Error while request to cve base")
-    return
+    return None
 
   if res['totalResults'] <= 0:
     print("Can't find %s"%(cve))
-    return
+    return None
+
   requestDate = res['timestamp']
   cve = res['vulnerabilities'][0]['cve']
   vulnStatus = cve['vulnStatus']
   name = cve['id']
+  sourceIdentifier = cve['sourceIdentifier']
   published = cve['published']
   published = published[0:10] + " " + published[11:19]
   description = cve['descriptions'][0]['value']
+  references = cve['references']
 
   metrics = cve['metrics']
   if len(metrics) >= 1:
@@ -102,6 +147,9 @@ def getCVEDataSelf(cve):
       metrics = metrics['cvssMetricV31'][0]['cvssData']
     elif 'cvssMetricV30' in metrics:
       metrics = metrics['cvssMetricV30'][0]['cvssData']
+    else:
+      print("Can't determine metric type")
+      return None
     attackVector = metrics['attackVector']
     attackComplexity = metrics['attackComplexity']
     baseScore = metrics['baseScore']
@@ -112,36 +160,108 @@ def getCVEDataSelf(cve):
     baseScore = 0.0
     baseSeverity = "unknown"
 
-  dbname = 'cve'
-  username = 'cve'
-  password = 'cve'
-  host = 'localhost'
+  result = {}
+  result["name"] = name
+  result["vuln_status"] = vulnStatus
+  result["source_identifier"] = sourceIdentifier
+  result["published"] = published
+  result["description"] = description
+  result["attack_vector"] = attackVector
+  result["attack_complexity"] = attackComplexity
+  result["base_score"] = baseScore
+  result["base_severity"] = baseSeverity
+  result["reference"] = references
+  result["reference"] = json.dumps(result["reference"])
 
-  sql = """insert into list (name, vuln_status, published, description, attack_vector, attack_complexity, base_score, base_severity)
-    values (%s, %s, %s, %s, %s, %s, %s, %s)"""
-  values = (name, vulnStatus, published, description, attackVector, attackComplexity, baseScore, baseSeverity)
+  return result
+
+def cveExistInTable(cveName, table):
+  sql = "select count(*) from " + table + " where name = '" + cveName + "'"
+  count = 0
+  try:
+    conn = psycopg2.connect(dbname=db, user=user, password=password, host=host)
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    count = cursor.fetchone()[0]
+    conn.commit()
+  except (Exception, Error) as error:
+    print(error)
+    cursor.close()
+    return True
+
+  if count > 0:
+    print("%s alredy added to database"%(cveName))
+    return True
+
+  return False
+
+def addToDatabase(cve, table):
+  sql = "insert into " + table + " (vuln_status_id, source_identifier_id, attack_vector_id, attack_complexity_id, base_severity_id, name, published, description, base_score, reference)"
+  sql +=  " select "
+  sql += "(select id from vuln_status where vuln_status.name = %s), "
+  sql += "(select id from source_identifier where name = %s), "
+  sql += "(select id from attack_vector where attack_vector.name = %s), "
+  sql += "(select id from attack_complexity where attack_complexity.name = %s), "
+  sql += "id, "
+  sql += "%s, "
+  sql += "%s, "
+  sql += "%s, "
+  sql += "%s, "
+  sql += "%s"
+  sql += "from base_severity where base_severity.name = %s"
+
+  values = (cve["vuln_status"],
+    cve["source_identifier"],
+    cve["attack_vector"],
+    cve["attack_complexity"],
+    cve["name"],
+    cve["published"],
+    cve["description"],
+    cve["base_score"],
+    cve["reference"],
+    cve["base_severity"])
 
   try:
-    conn = psycopg2.connect(dbname='cve', user='cve', password='cve', host='localhost')
+    conn = psycopg2.connect(dbname=db, user=user, password=password, host=host)
     cursor = conn.cursor()
     cursor.execute(sql, values)
     conn.commit()
   except (Exception, Error) as error:
     print(error)
-  finally:
     cursor.close()
-    print("%s add to database"%(name))
-    
-http_proxy  = "http://192.168.1.1:8080"
+    return
 
+  print("%s add to database"%(cve["name"]))
+
+db = "cve"
+user = "cve"
+password = "cve"
+host = "localhost"
+http_proxy  = "http://someproxy:8080"
 proxies = {
   "http"  : http_proxy,
   "https" : http_proxy
 }
+timeout = 6
 
-CVEList = getTopCVEList_cvetrends()
-for cve in CVEList:
-  print(cve + " processing ...")
-  getCVEDataSelf(cve)
-  #if future need sleep only after get new CVE data
-  time.sleep(5)
+#cvetrends not working!
+#print("Receiving cve data from cvetrends")
+#CVEList1 = getTopCVEList_cvetrends()
+
+print("Receiving cve data from opencve.io")
+CVEList = getTopCVEList_opencve()
+print("Receiving cve data from circl.lu")
+CVEList += getLastCVE_circl()
+print("Removing duplicates")
+CVEList = [*set(CVEList)]
+
+print("Adding to DB")
+for cveName in CVEList:
+  if cveExistInTable(cveName, 'list3') == True:
+    continue
+  cve = getCVEData(cveName)
+  if cve is None:
+    continue
+  addToDatabase(cve, 'list3')
+  time.sleep(timeout)
+print("Done")
